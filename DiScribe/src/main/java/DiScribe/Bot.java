2ef5d93a5d3c9ac099bd87d3ae7f66a1131ea1eb
Mustafa <mfitrat@masonlive.gmu.edu>
2020-10-06 15:22:52 -0400

    Testing example functionality

--- DiScribe/src/main/java/DiScribe/Bot.java
+++ DiScribe/src/main/java/DiScribe/Bot.java
@@ -2,14 +2,29 @@ package DiScribe;
 
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.User;
 
 public class Bot {
 
     public static void main(String[] args) {
-        GatewayDiscordClient client = DiscordClientBuilder.create("NzYzMTA4MzI0MjU3NjkzNzA2.X3y6Ag.vmb8F1wds6zAEBIbaIHRSmwgeHo")
-                .build()
-                .login()
-                .block();
+        GatewayDiscordClient client = DiscordClientBuilder.create("NzYzMTA4MzI0MjU3NjkzNzA2.X3y6Ag.vmb8F1wds6zAEBIbaIHRSmwgeHo").build().login().block();
+        
+        client.getEventDispatcher().on(ReadyEvent.class).subscribe(event -> {
+          User self = event.getSelf();
+          System.out.println(String.format("Logged in as %s#%s", self.getUsername(), self.getDiscriminator()));
+        });
+        
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+        .map(MessageCreateEvent::getMessage)
+        .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
+        .filter(message -> message.getContent().equalsIgnoreCase("!ping"))
+        .flatMap(Message::getChannel)
+        .flatMap(channel -> channel.createMessage("Pong!"))
+        .subscribe();
+        
         client.onDisconnect().block();
     }
 
