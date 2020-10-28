ddfc33ce69897d7f4b875e76df0c3c76941d205e
younsam <syoun5@gmu.edu>
2020-10-28 18:18:09 -0400

    Implemented basic methods, have not fully tested all of them

--- DiScribe/src/main/java/DiScribe/EventsList.java
+++ DiScribe/src/main/java/DiScribe/EventsList.java
@@ -5,49 +5,103 @@ import java.util.ArrayList;
 public class EventsList {
 	
 	private ArrayList<Event> list;
+	private ArrayList<String> nameList;
 	
 	public EventsList() {
 		list = new ArrayList<Event>();
-		
+		nameList = new ArrayList<String>();
 	}
 	
 
 	public boolean add(Event E) {
-		if(E == null)
-		{
+		if(E == null) {
 			return false;
 		}
-		
+		nameList.add(E.toString());
 		return list.add(E);
 	}
 	
-	public boolean remove(Event E)
-	{
-		if(E == null)
-		{
+	public boolean add(int index, Event E) {
+		if(E == null) {
 			return false;
 		}
-		
-
-		// Check to see if the Event is stored in the list
-		
+		nameList.add(index, E.toString());
+		list.add(index, E);
+		return true;
+	}
+	
+	public boolean remove(Event E) {
+		if(E == null) {
+			return false;
+		}
+		else if(list.contains(E) == false) {
+			return false;
+		}
+		nameList.remove(E.toString());
 		return list.remove(E);
 	}
 	
-	public Event get(Event E)
-	{
-		return list.get(list.indexOf(E));
-
+	public boolean contains(Event E) {
+		if(list.contains(E)) {
+			return true;
+		}
+		return false;
 	}
 	
-	public static void main(String []args)
-	{
+	public Event get(int index) {
+		if(index < 0 || index > list.size()) {
+			return null;  
+		}
+		else if(list.size() == 0) {
+			return null;
+		}
+		
+		return list.get(index);
+	}
+	
+	public int length() {
+		return list.size();
+	}
+	
+	public int find(Event E) {
+		if(E == null) {
+			return -1;
+		}
+		else if(list.size() == 0) {
+			return -1;
+		}
+		
+		return list.indexOf(E);
+	}
+	
+	public ArrayList<Event> ascAlphArray() {
+		return list;
+	}
+	
+	public ArrayList<Event> desAlphARray() {
+		return list;
+	}
+	
+	public ArrayList<Event> ascDateArray() {
+		return list;
+	}
+	
+	public ArrayList<Event> desDateArray() {
+		return list;
+	}
+	
+	public void displayList() {
+		// Displays list in string format
+		
+	}
+	
+	public static void main(String []args) {
 		Event bob = new Event("bob");
 		EventsList list = new EventsList();
 		
 		list.add(bob);
 
-		System.out.println(list.get(bob)); // Works
+		System.out.println(list.get(0)); // Works
 		
 		System.out.println(list.remove(bob));
 		System.out.println(list.remove(bob)); // Works
