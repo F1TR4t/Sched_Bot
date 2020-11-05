24e8ea023d6c4838a6f70534cf05cc9030b04961
yli61 <61408235+yli61@users.noreply.github.com>
2020-11-05 02:02:18 -0500

    Update Event.java

--- DiScribe/src/main/java/DiScribe/Event.java
+++ DiScribe/src/main/java/DiScribe/Event.java
@@ -1,14 +1,19 @@
 package DiScribe;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Event implements Comparable {
 	
-	private String name;
+        private String name;
 	private String desc;
 	private boolean active;
 	private String date;
-	private int start_time;
+	private String start_time;
+	private int EDT;
 	private String server;
 	private String channel;
 	private boolean recur;
@@ -40,8 +45,12 @@ public class Event implements Comparable {
 		return date;
 	}
 	
-	public int getstart_time() {
+	public String getstart_time() {
 		return start_time;
+	}
+	
+	public int getEDT() {
+		return EDT;
 	}
 	
 	public String getserver() {
@@ -86,6 +95,7 @@ public class Event implements Comparable {
 		this.name = name;
 	}
 	
+	
 	public void setdesc(String desc) {
 		if(desc.split("\\s+").length > 100) {
 			System.out.println("The desc should less than 100 words.");
@@ -93,8 +103,75 @@ public class Event implements Comparable {
 		this.desc = desc;
 	}
 	
+	
 	public void setactive(Boolean active) {
 		this.active = active;
+	}
+	
+	
+	public void setdate(String date) {
+		String regex = "[0-9]{4}-[0-9]{2}-[0-9]{2}";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher m = pattern.matcher(date);
+		boolean dateFlag = m.matches();
+		
+		if(!dateFlag) {
+			System.out.println("Date format error, it should be yyyy-mm-dd");
+			return;
+		}
+		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		formatter.setLenient(false);
+		try {
+			Date EventDate = formatter.parse(date);   //This is an important step to ensure that the date is correct
+			this.date = date;
+		}catch(Exception e) {
+			System.out.println("Date format error, it should be yyyy-mm-dd");
+		}
+	}
+	
+	
+	public void setstart_time(String time) {
+		this.start_time = time;
+	}
+	
+	
+	public void setEDT(int EDT) {
+		if(EDT <= 0) {
+			System.out.println("The estimate duration time should be greater than 0.");
+		}
+		else {
+			this.EDT = EDT;
+		}
+	}
+	
+	
+	public void setserver(String server) {
+		this.server = server;
+	}
+	
+	public void setchannel(String channel) {
+		this.channel = channel;
+	}
+	
+	
+	public void setrecur(Boolean recur) {
+		this.recur = recur;
+	}
+	
+	
+	public void setFrequency(String Frequency) {
+		if(getrecur() == false) {
+			System.out.println("The event will not recur.");
+		}
+		else {
+			if(Frequency.equals("biweekly")){ this.Frequency = Frequency; }
+			else if(Frequency.equals("weekly")) { this.Frequency = Frequency; }
+			else if(Frequency.equals("monthly")) {this.Frequency = Frequency; }
+			else if(Frequency.equals("daily")) {this.Frequency = Frequency; }
+			else {
+				System.out.println("You should input one of the {biweekly, weekly, monthly, daily}");
+			}
+		}
 	}
 	
 	@Override 
