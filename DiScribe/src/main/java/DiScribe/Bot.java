94db4c9514f77c5f227b9ef944e0825cd24bd6af
Mustafa <mfitrat@masonlive.gmu.edu>
2020-11-14 17:45:58 -0500

    Reworked the create and help functions.

--- DiScribe/src/main/java/DiScribe/Bot.java
+++ DiScribe/src/main/java/DiScribe/Bot.java
@@ -15,88 +15,76 @@ public class Bot {
 	
 	private static EventsList events;
 	private static int numEvents = 0;
-	private static String[] cmds = {"create", "remove", "edit", "list", "display"};
-	private static String[] attr = {"name", "desc", "active", "date", "duration", "server", "channel", "recur", "frequency", "max_frequency", "number_of_announcements"};
+	private static String[] cmds = {"help", "create", "remove", "edit", "list", "display"};
+	private static String[] attr = {"name", "desc", "active", "date", "duration", "server", "channel"};
 	
-	public static void process(String[] command, int len, MessageCreateEvent event) {
+	private static String  help = "List of Commands include:\n-------------------------\n"
+			+ "create [Name of Event]\ndelete [Name of Event]\nedit   [Name of Event]\n"
+			+ "display [Name of Event]\nlist\n\nIf you would like to know how a command works, write the command name then \"?\" after it.";
+	
+	private static String empty = "For a list of commands, use the \"help\" command.";
+
+	public static void process(String[] cmd, int len, MessageCreateEvent event) {
 		
-		// Make sure the command is not empty
-		if ( len <= 0 ) { return; }
+		// Might not be necessary, but in case of empty Strings
+		if (len == 0) {
+			event.getMessage()
+				.getChannel().block()
+				.createMessage(empty).block();
+		} 
 		
-		// Make sure the command has Keyword
-		if ( command[0] == null ) { return; } 
-		
-		if ( command[0].equals(cmds[0])) {
-			if ( (command.length == 2) && events.add(new Event(command[1])) ) { // If user specifies a name
-				numEvents++;
-				event.getMessage()
+		// For the help command
+		if (cmd[0].toLowerCase().equals(cmds[0])) {
+			event.getMessage()
 				.getChannel().block()
-				.createMessage("Created the event \"" + command[1] + "\"").block();
-			} else if (command.length == 1 && events.add(new Event("Event" + numEvents))) { // When user does not specify a name
-				event.getMessage()
-				.getChannel().block()
-				.createMessage("Created the event \"Event" + numEvents++ + "\"").block();
-			} else { // When they add too many arguments
-				event.getMessage()
-				.getChannel().block()
-				.createMessage("The create command requires you to name the event\n"
-						+ "Note: the name should be less than 50 characters.\n\n"
-						+ "```create [event_name]```\n\n"
-						+ "Example: Creating an event BBQ\n"
-						+ "```create BBQ```").block();
-			}
+				.createMessage(help).block();
 		}
 		
-		if ( command[0].equals(cmds[1])) {
-			if ( (command.length>1) && events.remove(new Event(command[1])) ) { // Specifies event to be removed
-				event.getMessage()
-				.getChannel().block()
-				.createMessage("Removed the event \"" + command[1] + "\"").block();
-			} else { // No Specification whatsoever
+		// for the Create Command
+		if (cmd[0].toLowerCase().equals(cmds[1])) {
+			
+			if ( len == 1 ) { 						// If they don't provide an Event Name
+				String genName = "Event#" + numEvents;
+				if ( events.add(new Event(genName)) ) { 		// Successful creation
+					event.getMessage()
+						.getChannel().block()
+						.createMessage("Created an event named \"Event#" + numEvents++ + "\"").block();
+				} else {										// Failure in creation
 					event.getMessage()
 					.getChannel().block()
-					.createMessage("The remove command requires you to name the event you want to remove\n\n"
-							+ "```remove [event_name]```\n\n"
-							+ "Example: If I created an event BBQ\n"
-							+ "```remove BBQ```").block();
+					.createMessage("Something went wrong when creating the event named \"Event#" + numEvents + "\"").block();
 				}
-		}
-		
-		/*if ( command[0].equals(cmds[3])) {
-			event.getMessage()
-			.getChannel().block()
-			.createMessage(events.displayList()).block();
-		}*/
-		
-		if ( command[0].equals(cmds[4])) { // Prone to Null Pointer Exceptions
-			if ( command.length == 2) {
-				event.getMessage()
-				.getChannel().block()
-				.createMessage("Displaying the event \"" + command[1] +"\"\n\n"
-					+ events.get(events.find(new Event(command[1]))).toString() ).block();
-			} else {
-				event.getMessage()
-				.getChannel().block()
-				.createMessage("The display command requires you to name the event you want to remove\n\n"
-						+ "```display [event_name]```\n\n"
-						+ "Example: If I created an event BBQ\n"
-						+ "```display BBQ```").block();
 			}
-		}
-		
-		if ( command[0].equals(cmds[2])) { // To be worked on
-			if ( command.length == 4 ) {
+			
+			if ( len > 1 ) { 						// If they do provide an event name
 				
-			}
-			else {
-			event.getMessage()
-			.getChannel().block()
-			.createMessage("The edit command requires you to name the event you want to edit and the attributes as well\n\n"
-					+ "```edit [event_name] [attribute] [desired_value]```\n\n"
-					+ "Example, if I want to change the event name of BBQ to Ketchup\n"
-					+ "```edit BBQ name Ketchup```").block();
+				if ( cmd[1].equals("?") ) { 			// Want to know more about the create method
+					event.getMessage()
+						.getChannel().block()
+						.createMessage("The \"create\" command is meant for creating events. It can either create a generic event if a name isn't given, or an event with a name provided by the user. Please see example below\n\n"
+							+ "```create\nCreated an event named \"Event#1\"\ncreate BBQ\nCreated an event named \"BBQ\"```\n\n").block();
+				} else { 								// They do have a name
+					String name = cmd[1];
+					for ( int i = 2; i < len; i++ ) {
+						name += " ";
+						name += cmd[i];
+					}
+					if ( events.add(new Event(name)) ) { 		// Successful creation
+						event.getMessage()
+							.getChannel().block()
+							.createMessage("Created an event named \"" + name + "\"").block();
+					} else { 									// Failure in creation
+							event.getMessage()
+						.getChannel().block()
+						.createMessage("Something went wrong when creating the event named \"" + name + "\"").block();
+					}
+				}
 			}
 		}
+		
+		
+		
+		
 	}
 
     public static void main(String[] args) {
